ce49b2c9b3229822132f44351d4290230bbd5f76
fazliddin24 <aminovfazliddin24@gmail.com>
2024-12-12 09:16:53 +0900

    ranglar

---
+++ rangli/script.js
@@ -0,0 +1,21 @@
+// let box = document.querySelectorAll('.box'),
+//     active = document.querySelector('.active'),
+//     counter = 0;
+//
+// box.forEach(item => {
+//     item.addEventListener('click', () => {
+//         active.classList.add('resize');
+//         if ( active.style.backgroundColor === item.innerText) {
+//             active.classList.remove('resize');
+//         }
+//         active.style.backgroundColor = item.innerText;
+//         removeClass();
+//         item.classList.add('box_active');
+//     });
+// });
+//
+// function removeClass() {
+//     box.forEach(box_item => {
+//         box_item.classList.remove('box_active');
+//     });
+// }
