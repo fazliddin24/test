46b3318290470f6731d1efc68718e976de56d903
fazliddin24 <aminovfazliddin24@gmail.com>
2024-12-17 08:55:02 +0900

    new

---
+++ carusel/script.js
@@ -0,0 +1,13 @@
+//
+// function enlargeImage(img) {
+//     const overlay = document.getElementById('overlay');
+//     const overlayImage = document.getElementById('overlay-image');
+//
+//     overlayImage.src = img.src; // Set enlarged image source
+//     overlay.style.display = 'flex'; // Show the overlay
+// }
+//
+// function closeImage() {
+//     const overlay = document.getElementById('overlay');
+//     overlay.style.display = 'none';
+// }
