85e5baa6b4ba29e2840ce53adaff3563779ec7d3
fazliddin24 <aminovfazliddin24@gmail.com>
2024-12-16 10:00:48 +0900

    new code

--- rangli/script.js
+++ rangli/script.js
@@ -19,3 +19,21 @@
 //         box_item.classList.remove('box_active');
 //     });
 // }
+
+let box = document.querySelectorAll('.box');
+let active = document.querySelector('.active');
+
+box.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        removeClass();
+        let nxtIndex = (index + 1) % box.length;
+        box[nxtIndex].style.transform = `translateY(-100px)`;
+    });
+});
+
+function removeClass() {
+    box.forEach(box_item => {
+        box_item.style.transform = 'translateY(0)';
+    });
+}
+
