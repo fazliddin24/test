c9d9619b2e8bccb3b153302fd5b19159ce42c55d
fazliddin24 <aminovfazliddin24@gmail.com>
2024-12-17 09:18:49 +0900

    new

--- carusel/script.js
+++ carusel/script.js
@@ -11,3 +11,16 @@
 //     const overlay = document.getElementById('overlay');
 //     overlay.style.display = 'none';
 // }
+
+
+const images = ["2.jpg","3.jpg","4.jpg","MainAfter.webp", "White%20&%20Blue%20Elegant%20Modern%20Participation%20Certificate.png", "dol.jpg"];
+let currentIndex = 0;
+const carouselImage = document.getElementById("carousel-image");
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    carouselImage.src = images[currentIndex];
+}
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    carouselImage.src = images[currentIndex];
+}
