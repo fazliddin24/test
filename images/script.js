c3a2ec49cf73a9ab0ab3b3d618659dc1cd5eb6e8
fazliddin24 <aminovfazliddin24@gmail.com>
2024-12-19 11:43:32 +0900

    new

--- images/script.js
+++ images/script.js
@@ -1,21 +1,37 @@
-const images = document.querySelectorAll('.gallery img');
+const images = document.querySelectorAll('.corner-images img');
 const popup = document.getElementById('popup');
 const popupImage = document.getElementById('popup-image');
 const closeBtn = document.getElementById('close-btn');
+const activeImage = document.getElementById('active-image');
+const activeImageTag = activeImage.querySelector('img');
 
+let currentIndex = 0;
+function changeActiveImage() {
+    const src = images[currentIndex].getAttribute('src');
+    activeImageTag.setAttribute('src', src);
+}
 images.forEach(image => {
-    image.addEventListener('click', () => {
-        popupImage.src = image.src;
+    activeImage.addEventListener('click', () => {
+        popupImage.src = activeImage.querySelector('img').src
         popup.classList.add('visible');
     });
 });
-
 closeBtn.addEventListener('click', () => {
     popup.classList.remove('visible');
 });
-
 popup.addEventListener('click', (e) => {
     if (e.target !== popupImage && e.target !== closeBtn) {
         popup.classList.remove('visible');
     }
 });
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        const src = image.getAttribute('src');
+        activeImageTag.setAttribute('src', src);
+        activeImage.style.display = 'flex';
+    });
+});
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeActiveImage();
+}, 2000);
