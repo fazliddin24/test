3fa39a851ebc9e55e0c2e27bc701c43493e6a110
fazliddin24 <aminovfazliddin24@gmail.com>
2025-02-20 09:27:08 +0300

    v

--- backend/1-lesson/app.py
+++ backend/1-lesson/app.py
@@ -48,7 +48,7 @@
 #     print(item)
 
 #
-# if 4 in list_numbers:
+# if 4 not in list_numbers:
 #     print(True)
 
 # number = int(input('Son kiriting: '))
@@ -57,16 +57,104 @@
 #     number = int(input('Son kiriting: '))
 
 
-list_numbers = [12, 4, 32, 44, 23, 4]
-print('[12, 4, 32, 44, 23, 4]')
-target = int(input('sonlardan birini kiriting: '))
-found = False
-index = 0
-i = 0
-while i < len(list_numbers):
-    if list_numbers[i] == target:
-        index = i
-        found = True
-    i += 1
-if found == True:
-    print(f"Element {target} indeksda: {index}")
+# list_numbers = [12, 4, 32, 44, 23, 4]
+# target = int(input('son kiriting: '))
+# found = False
+# index = 0
+# i = 0
+# while i < len(list_numbers):
+#     if list_numbers[i] == target:
+#         index = i
+#         found = True
+#     i += 1
+# if found == True:
+#     print(f"Element {target} indeksda: {index}")
+# else:
+#     print('kiritilgan son mavjud emas')
+
+# list_numbers = [12, 12, 12, 44, 4, 32, 44, 23, 4]
+# target = int(input('Son kiriting: '))
+# count = 0
+#
+# for num in list_numbers:
+#     if num == target:
+#         count += 1
+# print(f"{target} soni ro'yxatda {count} marta uchraydi.")
+
+
+# list_numbers = [12, 12, 12, 44, 4, 32, 44, 23, 4]
+# num2 = []
+#
+# for num in list_numbers:
+#     if num not in num2:
+#         count = 0
+#         for son in list_numbers:
+#             if son == num:
+#                 count += 1
+#         print(f"{num} soni ro'yxatda {count} marta uchraydi.")
+#         num2.append(num)
+
+
+
+# def plus(number1, number2):
+#     return number1 + number2, 16
+#
+#
+# number = plus(number2=12, number1=3)
+# print(number)
+
+
+ishchi = {
+    "ism": 'SHerbek',
+    "oylik": 1500,
+}
+
+# def oylik_hisob():
+#     oy = int(input("Neca oy ishladi ? "))
+#     jami = 0
+#     print(range(1, oy + 1))
+#     for i in range(1, oy + 1):
+#         jami += ishchi["oylik"]
+#     print(f"{ishchi['ism']} {oy} oy jami {jami}$ ish haqi oladi.")
+# oylik_hisob()
+
+def oylik_hisob():
+    oy = int(input("Necha oy ishladi? "))
+    jami = 0
+    i = 1
+    while i <= oy:
+        jami += ishchi["oylik"]
+        i += 1
+    print(f"{ishchi['ism']} {oy} oy jami {jami}$ ish haqi oladi.")
+oylik_hisob()
+
+#
+# def qosh(x, y):
+#     return x + y
+# def ayir(x, y):
+#     return x - y
+# def kop(x, y):
+#     return x * y
+# def bol(x, y):
+#     if y == 0:
+#         return "Nolga bolib bolmaydi j*@#*"
+#     return x / y
+# def calc():
+#
+#     x = float(input("Birinchi sonni kirit: "))
+#     am = input("Amalni yoz: ")
+#     y = float(input("Ikkinchi sonni kirit: "))
+#     if am == '+':
+#         print("Natija:", qosh(x, y))
+#     elif am == '-':
+#         print("Natija:", ayir(x, y))
+#     elif am == '*':
+#         print("Natija:", kop(x, y))
+#     elif am == '/':
+#         print("Natija:", bol(x, y))
+#     else:
+#         print("Noto'g'ri amal!")
+# calc()
+#
+#
+#
